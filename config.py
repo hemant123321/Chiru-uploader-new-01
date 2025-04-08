f5436541709194e282effd30f1341247246c5c2e
hemant123321 <subhashgodara237057@gmail.com>
2025-04-08 13:15:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,16 +8,16 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 6326227068))
 
-#LOG = -1002159628443,
+#LOG = -1002619524750,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
+    #ADMINS=[6326227068]
     #for x in (os.environ.get("ADMINS", "7827463899").split()):
         #ADMINS.append(int(x))
 #except ValueError:
